e4691ea7ae19167cd0e401a03029653e685cb42c
saritagud <gudinop864@gmail.com>
2022-03-20 14:33:02 -0430

    https://github.com/Avendaosander/Proyecto-BACKEND.git

---
+++ controllers/classPublicaciones.js
@@ -0,0 +1,27 @@
+const modeloPublicaciones = require('../db/publicaiones');
+
+class Publicaciones{
+    constructor(data){
+        this.Nombre = data.Nombre
+        this.Apellido= data.Apellido
+        this.Cedula= data.Cedula
+        this.titulo = data.titulo
+        this.Contenido= data.Contenido
+        this.Contador = 0;
+        this.modelo = modeloPublicaciones
+    }
+
+    AgreagarPublicacion(){
+        this.modelo.create({
+            Nombre : this.Nombre,
+            Apellido: this.Apellido,
+            Cedula: this.Cedula,
+            Titulo: this.titulo,
+            Contenido: this.Contenido,
+            Contador : this.Contador, 
+        },{
+            fields:['Nombre', 'Apellido', 'Cedula', 'Titulo','Contenido', 'Contador']
+        })
+    }
+}
+module.exports.Publicaciones=Publicaciones;
